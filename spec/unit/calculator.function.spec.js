d22f6bd6af4be522a382b0256c44f9b737121667
HRanjan-11 <106653965+HRanjan-11@users.noreply.github.com>
2022-07-27 01:27:20 +0530

    Update calculator.function.spec.js

--- spec/unit/calculator.function.spec.js
+++ spec/unit/calculator.function.spec.js
@@ -16,135 +16,135 @@ const calc = require('../../lib/calculator.functions');
 
 describe("Calculator Functions", function() {
 
-  // add
+//   // add
 
-  describe("add", function() {
+//   describe("add", function() {
 
-    it("should add two positive numbers", function() {
-      let value = calc.add(3, 2);
-      expect(value).toBe(5);
-    });
+//     it("should add two positive numbers", function() {
+//       let value = calc.add(3, 2);
+//       expect(value).toBe(5);
+//     });
   
-    it("should add a positive and a negative number", function() {
-      let value = calc.add(3, -2);
-      expect(value).toBe(1);
-    });
+//     it("should add a positive and a negative number", function() {
+//       let value = calc.add(3, -2);
+//       expect(value).toBe(1);
+//     });
   
-    it("should give the same value when adding zero", function() {
-      let value = calc.add(3, 0);
-      expect(value).toBe(3);
-    });
+//     it("should give the same value when adding zero", function() {
+//       let value = calc.add(3, 0);
+//       expect(value).toBe(3);
+//     });
   
-  });
-
-
-  // subtract
-
-  describe("subtract", function() {
-
-    it("should subtract two numbers", function() {
-      let value = calc.subtract(3, 2);
-      expect(value).toBe(1);
-    });
-  
-    it("should subtract two numbers for a negative result", function() {
-      let value = calc.subtract(2, 3);
-      expect(value).toBe(-1);
-    });
-  
-    it("should give the same value when subtracting zero", function() {
-      let value = calc.subtract(3, 0);
-      expect(value).toBe(3);
-    });
-  
-  });
-  
-
-  // multiply
-
-  describe("multiply", function() {
-
-    it("should multiply two positive numbers", function() {
-      let value = calc.multiply(3, 2);
-      expect(value).toBe(6);
-    });
-  
-    it("should multiply two negative numbers", function() {
-      let value = calc.multiply(-3, -2);
-      expect(value).toBe(6);
-    });
-  
-    it("should multiply a positive and a negative", function() {
-      let value = calc.multiply(3, -2);
-      expect(value).toBe(-6);
-    });
-  
-    it("should give the same value when multiplying by one", function() {
-      let value = calc.multiply(5, 1);
-      expect(value).toBe(5);
-    });
-  
-    it("should give zero when multiplying by zero", function() {
-      let value = calc.multiply(5, 0);
-      expect(value).toBe(0);
-    });
-  
-  });
-  
-
-  // divide
-
-  describe("divide", function() {
-
-    it("should divide two positive numbers", function() {
-      let value = calc.divide(10, 2);
-      expect(value).toBe(5);
-    });
-  
-    it("should divide two negative numbers", function() {
-      let value = calc.divide(-10, -2);
-      expect(value).toBe(5);
-    });
-  
-    it("should divide a positive and a negative", function() {
-      let value = calc.divide(-10, 2);
-      expect(value).toBe(-5);
-    });
-  
-    it("should divide two positive numbers with a decimal result", function() {
-      let value = calc.divide(3, 2);
-      expect(value).toBe(1.5);
-    });
-  
-    it("should give the same value when dividing by one", function() {
-      let value = calc.divide(5, 1);
-      expect(value).toBe(5);
-    });
-  
-    it("should throw an exception when dividing by zero", function() {
-      let divideByZero = function() { calc.divide(3, 0); };
-      expect(divideByZero).toThrowError(RangeError, 'Divide-by-zero');
-    });
-  
-  });
+//   });
 
 
-  // maximum
+//   // subtract
 
-  describe("maximum", function() {
+//   describe("subtract", function() {
 
-    [
-      [1, 2, 2],
-      [2, 1, 2],
-      [2, 2, 2],
-    ].forEach(([a, b, expected]) => {
-      it(`should return ${expected} when given ${a} and ${b}`, () => {
-        let value = calc.maximum(a, b);
-        expect(value).toBe(expected);
-      });
-    });
+//     it("should subtract two numbers", function() {
+//       let value = calc.subtract(3, 2);
+//       expect(value).toBe(1);
+//     });
+  
+//     it("should subtract two numbers for a negative result", function() {
+//       let value = calc.subtract(2, 3);
+//       expect(value).toBe(-1);
+//     });
+  
+//     it("should give the same value when subtracting zero", function() {
+//       let value = calc.subtract(3, 0);
+//       expect(value).toBe(3);
+//     });
+  
+//   });
+  
 
-  });
+//   // multiply
+
+//   describe("multiply", function() {
+
+//     it("should multiply two positive numbers", function() {
+//       let value = calc.multiply(3, 2);
+//       expect(value).toBe(6);
+//     });
+  
+//     it("should multiply two negative numbers", function() {
+//       let value = calc.multiply(-3, -2);
+//       expect(value).toBe(6);
+//     });
+  
+//     it("should multiply a positive and a negative", function() {
+//       let value = calc.multiply(3, -2);
+//       expect(value).toBe(-6);
+//     });
+  
+//     it("should give the same value when multiplying by one", function() {
+//       let value = calc.multiply(5, 1);
+//       expect(value).toBe(5);
+//     });
+  
+//     it("should give zero when multiplying by zero", function() {
+//       let value = calc.multiply(5, 0);
+//       expect(value).toBe(0);
+//     });
+  
+//   });
+  
+
+//   // divide
+
+//   describe("divide", function() {
+
+//     it("should divide two positive numbers", function() {
+//       let value = calc.divide(10, 2);
+//       expect(value).toBe(5);
+//     });
+  
+//     it("should divide two negative numbers", function() {
+//       let value = calc.divide(-10, -2);
+//       expect(value).toBe(5);
+//     });
+  
+//     it("should divide a positive and a negative", function() {
+//       let value = calc.divide(-10, 2);
+//       expect(value).toBe(-5);
+//     });
+  
+//     it("should divide two positive numbers with a decimal result", function() {
+//       let value = calc.divide(3, 2);
+//       expect(value).toBe(1.5);
+//     });
+  
+//     it("should give the same value when dividing by one", function() {
+//       let value = calc.divide(5, 1);
+//       expect(value).toBe(5);
+//     });
+  
+//     it("should throw an exception when dividing by zero", function() {
+//       let divideByZero = function() { calc.divide(3, 0); };
+//       expect(divideByZero).toThrowError(RangeError, 'Divide-by-zero');
+//     });
+  
+//   });
+
+
+//   // maximum
+
+//   describe("maximum", function() {
+
+//     [
+//       [1, 2, 2],
+//       [2, 1, 2],
+//       [2, 2, 2],
+//     ].forEach(([a, b, expected]) => {
+//       it(`should return ${expected} when given ${a} and ${b}`, () => {
+//         let value = calc.maximum(a, b);
+//         expect(value).toBe(expected);
+//       });
+//     });
+
+//   });
 
   
   // minimum
